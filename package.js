7cd50bf5513839adb69e0a6de59c631f21814593
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2014-12-07 18:41:40 +0100

    Bumps version to 1.1.1

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'sanjo:karma',
   summary: 'Integrates Karma into Meteor',
-  version: '1.1.0',
+  version: '1.1.1',
   git: 'https://github.com/Sanjo/meteor-karma.git'
 })
 
