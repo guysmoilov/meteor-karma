c92816b76982c11a204e4dc974706a008b8605a0
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2015-01-18 21:55:09 +0100

    Release of 1.2.0
    Adds Firefox and Sauce Labs Karma packages.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'sanjo:karma',
   summary: 'Integrates Karma into Meteor',
-  version: '1.1.3',
+  version: '1.2.0',
   git: 'https://github.com/Sanjo/meteor-karma.git'
 })
 
