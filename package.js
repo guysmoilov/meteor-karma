3e9942c17ca9ce863e7a4c64d561f1e5a0e8cebc
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2015-01-31 22:37:21 +0100

    Updates karma and karma-jasmine dependencies

--- package.js
+++ package.js
@@ -1,15 +1,15 @@
 Package.describe({
   name: 'sanjo:karma',
   summary: 'Integrates Karma into Meteor',
-  version: '1.2.0',
+  version: '1.3.0',
   git: 'https://github.com/Sanjo/meteor-karma.git'
 })
 
 Npm.depends({
-  'karma': '0.12.28',
+  'karma': '0.12.31',
   'karma-chrome-launcher': '0.1.7',
   'karma-firefox-launcher': '0.1.4',
-  'karma-jasmine': '0.3.2',
+  'karma-jasmine': '0.3.5',
   'karma-coffee-preprocessor': '0.2.1',
   'karma-phantomjs-launcher-nonet': '0.1.3',
   'karma-sauce-launcher': '0.2.10',
