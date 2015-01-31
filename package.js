46525e3155ddfd8345a1c0b11fc9591026067036
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2015-01-31 23:05:09 +0100

    Fixes that not the newest Karma version was used
    Needed to delete the .npm folder for Meteor to pick it up

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'sanjo:karma',
   summary: 'Integrates Karma into Meteor',
-  version: '1.3.0',
+  version: '1.3.1',
   git: 'https://github.com/Sanjo/meteor-karma.git'
 })
 
